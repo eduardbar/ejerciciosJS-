3383a8b4ac2f0ab284b161c5686ac2330f9706ed
eduardbar <119897290+eduardbar@users.noreply.github.com>
2024-04-15 12:15:21 -0500

    feat: :tada: sintaxis basica de JS
    Ejercicios para aprender js con node

---
+++ js/main.js
@@ -0,0 +1,6 @@
+import prompt from "prompt-sync"
+let input = prompt();
+let nombre = input("ingrese el nombre :")
+let edad = Number(input("ingrese la edad :"))
+console.log(nombre)
+console.log(edad)
